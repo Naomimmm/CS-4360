53ec37d10baa6550474585ec36066bbfcd2cc999
Noemi Marquez <njupiter000@gmail.com>
2022-02-21 12:14:32 -0700

    added gitignore file

--- onestopbooks/onestopbooks/settings.py
+++ onestopbooks/onestopbooks/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY =
+SECRET_KEY = 'aihnu4x1)shyip%5*iexh)^&0-)ngwb-=#$$#%p9b1!8=tu(ff'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
