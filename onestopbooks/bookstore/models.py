2180a3ece919cec968158640f8753f7374b8d5f8
HuyBui-ruum <huybui.ruum@gmail.com>
2022-03-01 21:39:07 -0700

    add quantity and price to book models

--- onestopbooks/bookstore/models.py
+++ onestopbooks/bookstore/models.py
@@ -3,11 +3,14 @@ from django.utils.translation import gettext as _
 
 # Create your models here.
 class Book(models.Model):
+    """ Book model with ISBN, title, authors, year public, price, quantity and thumbnail"""
     isbn = models.CharField(_("isbn"), max_length = 30)
     title = models.CharField(_("title"), max_length = 100)
     authors = models.CharField(_("authors"), max_length = 100)
     year_public = models.IntegerField(_("year_public"), null = True)
     publisher = models.CharField(_("publisher"), max_length = 100, null = True)
+    price = models.IntegerField(_("price"), max_length = 10)
+    quantity = models.IntegerField(_("Quantity"), max_length = 10)  
     thumbnail_pic = models.ImageField(_("thumbnail_pic"), null = True, blank = True, upload_to ="static/images/books")
 
     def __str__(self):
