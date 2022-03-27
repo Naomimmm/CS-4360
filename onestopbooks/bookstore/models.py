ab3710ad90215b890a5d3949ea68d41ca115ce0d
HuyBui-ruum <huybui.ruum@gmail.com>
2022-03-27 10:19:30 -0600

    add to cart- buy & rent - order summary

--- onestopbooks/bookstore/models.py
+++ onestopbooks/bookstore/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils.translation import gettext as _
-
+from datetime import datetime, timedelta
+from django.contrib.auth.models import User
 # Create your models here.
 class Book(models.Model):
     """ Book model with ISBN, title, authors, year public, price, quantity and thumbnail"""
@@ -10,22 +11,86 @@ class Book(models.Model):
     year_public = models.IntegerField(_("year_public"), null = True)
     publisher = models.CharField(_("publisher"), max_length = 100, null = True)  
     thumbnail_pic = models.ImageField(_("thumbnail_pic"), null = True, blank = True, upload_to ="static/images/books")
-    price = models.IntegerField(_("price"), max_length = 10)
-    quantity = models.IntegerField(_("Quantity"), max_length = 10)
+    price = models.IntegerField(_("price"))
+    quantity = models.IntegerField(_("Quantity"))
 
     def __str__(self):
         """String for representing the Book title."""
         return self.title
+    
+    # @property
+    # def updateInventory(self):
+    #     """ To decrease item quantity after checkout """
+    #     orderitems = Order.orderitem_set.all()
+    #     rentitems = Order.rentitem_set.all()
+    #     totalbuy = sum([item.quantity for item in orderitems])
+    #     totalrent = sum([item.quantity1 for item in rentitems])
+    #     total = totalbuy + totalrent
+    #     return total
+    
+    # def remaining_stock(self):
+    #     return self.quantity - Order.get_cart_total
+        
 
 # Customer Model
 class Customer(models.Model):
     """A customer model to store basic identifying information for customer end users."""
 
     # Fields
+    user = models.OneToOneField(User, on_delete=models.CASCADE, null=True, blank=True)
     first_name = models.CharField(max_length = 30)
     last_name = models.CharField(max_length = 30)
     email = models.EmailField()
     address_1 = models.CharField(_("address"), max_length = 128)
     city = models.CharField(max_length = 128)
     state = models.CharField(max_length = 128)
-    zip_code = models.CharField(max_length = 5) 
+    zip_code = models.CharField(max_length = 5)
+    
+    def __str__(self):
+        """String for representing the Book title."""
+        return self.last_name
+
+class Order(models.Model):
+    """ Cart/Order model """
+    customer = models.ForeignKey(Customer, on_delete = models.SET_NULL, blank=True, null=True) #ForeignKey => so one to many relationship which one customer can have many orders
+    date_order = models.DateTimeField(auto_now_add=True) #When order created
+    complete = models.BooleanField(default=False, null=True, blank=False) # if complete is false then customer can continue to adding items to that cart
+    transaction_id = models.CharField(max_length=200, null=True) # add some extra info to order
+    
+    def __str__(self):
+        return str(self.id)
+    @property
+    # get total price for cart
+    def get_cart_total(self):
+        orderitems = self.orderitem_set.all()
+        total = sum([item.get_total for item in orderitems])
+        return total
+
+    @property
+    def get_cart_items(self):
+        orderitems = self.orderitem_set.all()
+        rentitems = self.rentitem_set.all()
+        totalbuy = sum([item.quantity for item in orderitems])
+        totalrent = sum([item.quantity1 for item in rentitems])
+        total = totalbuy + totalrent
+        return total
+class OrderItem(models.Model):
+    """ Cart can have multiple item thats why we use foregnkey relationship """
+    product = models.ForeignKey(Book, on_delete=models.SET_NULL, blank=True, null=True)
+    order = models.ForeignKey(Order, on_delete=models.SET_NULL, blank=True, null=True)
+    quantity = models.IntegerField(default=0, null=True, blank=True) # quantity of that book in cart, default is at 0
+    date_added = models.DateTimeField(auto_now_add=True)
+    
+    @property
+    # get total prices in cart for that book
+    def get_total(self):
+        total = self.product.price * self.quantity
+        return total
+    
+class RentItem(models.Model):
+    """ Cart can have multiple item thats why we use foregnkey relationship """
+    product1 = models.ForeignKey(Book, on_delete=models.SET_NULL, blank=True, null=True)
+    order1 = models.ForeignKey(Order, on_delete=models.SET_NULL, blank=True, null=True)
+    quantity1 = models.IntegerField(default=0, null=True, blank=True) # quantity of that book in cart, default is at 0
+    date_added1 = models.DateTimeField(auto_now_add=True)
+    date_due1 = datetime.now() + timedelta(days=7)
--- onestopbooks/bookstore/views.py
+++ onestopbooks/bookstore/views.py
@@ -4,7 +4,11 @@ from django.contrib.auth import login, logout, authenticate
 from django.views import generic
 from bookstore.models import *
 from .forms import *
-
+from django.http import JsonResponse
+import json
+from django.forms import inlineformset_factory
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
 # Create your views here.
 def home_view(request, *args, **kwargs):
     return render(request, "home.html", {})
@@ -12,7 +16,7 @@ def home_view(request, *args, **kwargs):
 
 def books_view(request, *args, **kwargs):
     results = request.POST.get('book-filterd')
-
+    
     if results == 'featured':
         books = Book.objects.all()
     elif results == 'titles_az':
@@ -34,11 +38,40 @@ def aboutus_view(request, *args, **kwargs):
 
 
 def checkout_view(request, *args, **kwargs):
-    return render(request, "checkout.html", {})
+    # check if user is authenticated
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer = customer, complete=False) # create object or quere one, if object isnt exist then we will create it
+        #rent, created = Order.objects.get_or_create(customer = customer, complete=False) # create object or quere one, if object isnt exist then we will create it
+        items = order.orderitem_set.all() # this is for purchase
+        rents = order.rentitem_set.all() # this is for rent
+    else: # for user that isnt log in
+        items = [] # empty for now
+        rents = []
+        order = {'get_cart_total':0, 'get_cart_items':0}
+        # rent = {'get_cart_total':0, 'get_cart_items':0}
+    context = {'items':items, 'rents':rents, 'order':order} # 'order':order}       #'rents':rents, 'rent':rent
+    
+    return render(request, "checkout.html", context)
 
 
 def cart_view(request, *args, **kwargs):
-    return render(request, "cart.html", {})
+    # check if user is authenticated
+    if request.user.is_authenticated:
+        customer = request.user.customer
+
+        buy, created = Order.objects.get_or_create(customer = customer, complete=False) # create object or quere one, if object isnt exist then we will create it
+        rent, created = Order.objects.get_or_create(customer = customer, complete=False) # create object or quere one, if object isnt exist then we will create it
+        items = buy.orderitem_set.all() # this is for purchase
+        rents = rent.rentitem_set.all() # this is for rent
+    else: # for user that isnt log in
+        items = [] # empty for now
+        rents = []
+        buy = {'get_cart_total':0, 'get_cart_items':0}
+        rent = {'get_cart_total':0, 'get_cart_items':0}
+    context = {'items':items, 'rents':rents, 'buy':buy, 'rent':rent}
+    return render(request, "cart.html", context)
+    
 
 
 def loginPage(request):
@@ -52,6 +85,9 @@ def loginPage(request):
         if user is not None:
             login(request,user)
             return redirect('/')
+        else:
+            messages.info(request, 'Username Or Password is incorrect')
+            
        context={}
        return render(request,'login.html',context)
 
@@ -60,11 +96,11 @@ def signupPage(request):
     if request.user.is_authenticated:
         return redirect('home') 
     else: 
-        form=createuserform()
-        cust_form=createcustomerform()
+        form=CreateUserForm()
+        cust_form=CustomerForm()
         if request.method=='POST':
-            form=createuserform(request.POST)
-            cust_form=createcustomerform(request.POST)
+            form=CreateUserForm(request.POST)
+            cust_form=CustomerForm(request.POST)
             if form.is_valid() and cust_form.is_valid():
                 user=form.save()
                 customer=cust_form.save(commit=False)
@@ -76,6 +112,26 @@ def signupPage(request):
             'cust_form':cust_form,
         }
         return render(request,'signup.html',context)
+    # if request.user.is_authenticated:
+    #     return redirect('home')
+    # else:
+    #     form = CreateUserForm()
+    #     if request.method == 'POST':
+    #         form = CreateUserForm(request.POST)
+    #         if form.is_valid():
+    #             user = form.save()
+    #             #username = form.cleaned_data.get('username')
+                
+    #             Customer.objects.create(
+    #                 user=user,
+    #                 #name=user.username,
+    #             )
+    #             #messages.success(request, f'Account created for {username}!')
+    #             return redirect('login')
+    #     # else:
+    #     #     form = UserRegisterForm()
+    # context = {'form':form}
+    # return render(request, 'signup.html', context)
 
 
 def logoutPage(request):
@@ -86,4 +142,45 @@ def logoutPage(request):
 def product_view(request, isbn):
     book = Book.objects.get(isbn = isbn)
     return render(request, "product.html", {'book': book})
+
+def updateItem(request):
+    data = json.loads(request.body)
+    bookIsbn = data['bookIsbn']
+    action = data['action']
+    # print out action and isbn in command prompt just to test
+    print('Action:', action)
+    print('bookIsbn:', bookIsbn)
+    
+    customer = request.user.customer
+    #product = Book.objects.get(isbn=bookIsbn)
+    product1 = Book.objects.get(isbn=bookIsbn)
+    order1, created = Order.objects.get_or_create(customer=customer, complete=False)    
+    #order2, created = Order.objects.get_or_create(customer)
+    
+    rentItem, created = RentItem.objects.get_or_create(order1 = order1, product1=product1)
+    orderItem, created = OrderItem.objects.get_or_create(order = order1, product=product1)
+    
+    if action == 'purchase':
+        orderItem.quantity = (orderItem.quantity + 1)
+    if action == 'add':
+        orderItem.quantity = (orderItem.quantity + 1)    
+    elif action == 'remove':
+        orderItem.quantity = (orderItem.quantity - 1)    
+    orderItem.save()
+    
+    if action =='rent':
+        rentItem.quantity1 = (rentItem.quantity1 + 1)
+    rentItem.save()
+    
+    
+    if action =='delete':
+        orderItem.delete() 
+    if orderItem.quantity <= 0:
+        orderItem.delete()
+          
+    if action =='deleteRent':
+        rentItem.delete()    
+    if rentItem.quantity1 <= 0:
+        rentItem.delete()
+    return JsonResponse('Item was added', safe=False)
 
