bff4d4ce62634bf5315921d05ba4d23cb2d051f5
HuyBui-ruum <huybui.ruum@gmail.com>
2022-05-08 08:58:35 -0600

    fix broken filter because of paginator

--- onestopbooks/bookstore/views.py
+++ onestopbooks/bookstore/views.py
@@ -82,22 +82,31 @@ def books_view(request, *args, **kwargs):
     
     results = request.POST.get('book-filterd')
     
+    books = Book.objects.all()
+    p = Paginator(Book.objects.all(), 20)
+    page = request.GET.get('page')
+    book_page = p.get_page(page) 
     if results == 'featured':
-        books = Book.objects.all()
+        p = Paginator(Book.objects.all(), 20)        
+        
     elif results == 'titles_az':
-        books = Book.objects.all().order_by('title')
+        p = Paginator(Book.objects.all().order_by('title'), 20)
+        book_page = p.get_page(page) 
+        
     elif results == 'authors_az':
-        books = Book.objects.all().order_by('authors')
+        p = Paginator(Book.objects.all().order_by('authors'), 20)
+        book_page = p.get_page(page) 
+        
     elif results == 'price_lh':
-        books = Book.objects.all().order_by('price')
+        p = Paginator(Book.objects.all().order_by('price'), 20)
+        book_page = p.get_page(page) 
+        
     elif results == 'price_hl':
-        books = Book.objects.all().order_by('-price')
-    else:
-        books = Book.objects.all()
-        # Set up Pagination for book page
-        p = Paginator(Book.objects.all(), 20)
-        page = request.GET.get('page')
-        book_page = p.get_page(page)        
+        p = Paginator(Book.objects.all().order_by('-price'), 20)
+        book_page = p.get_page(page) 
+        
+    else:        
+        p = Paginator(Book.objects.all(), 20)       
     context = {'items':items, 'buy':buy, 'books':books, 'book_page':book_page}
 
     return render(request, "products.html", context)
